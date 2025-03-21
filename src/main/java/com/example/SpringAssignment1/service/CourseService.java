7f0b670a32ff6c75afa2c6669841cd68f4eee262
CJ-Mampane <cathrenejabulilesm@gmail.com>
2025-03-21 20:41:59 +0200

    Added predefined courses in CourseService

--- src/main/java/com/example/SpringAssignment1/service/CourseService.java
+++ src/main/java/com/example/SpringAssignment1/service/CourseService.java
@@ -11,19 +11,19 @@ public class CourseService {
 
     // Constructor to add courses when the application starts
     public CourseService() {
-        courses.add(new Course("Introduction to Programming", "Foundation"));
-        courses.add(new Course("Computer Organization", "Foundation"));
+        courses.add(new Course("CSC111F Computer literacy for science", "Foundation"));
+        courses.add(new Course("CSC121F Elementary computer programming", "Foundation"));
 
-        courses.add(new Course("Data Structures", "Undergraduate"));
-        courses.add(new Course("Operating Systems", "Undergraduate"));
-        courses.add(new Course("Database Systems", "Undergraduate"));
-        courses.add(new Course("Computer Networks", "Undergraduate"));
-        courses.add(new Course("Artificial Intelligence", "Undergraduate"));
+        courses.add(new Course("CSC113 Introduction to computing and programming concepts", "Undergraduate"));
+        courses.add(new Course("CSC121 Elementary computer programming", "Undergraduate"));
+        courses.add(new Course("CSC211 Advanced programming", "Undergraduate"));
+        courses.add(new Course("CSC212 Computer architecture and organization", "Undergraduate"));
+        courses.add(new Course("CSC223 Data structures and algorithms", "Undergraduate"));
 
-        courses.add(new Course("Advanced Machine Learning", "Honours"));
-        courses.add(new Course("Cybersecurity", "Honours"));
-        courses.add(new Course("Cloud Computing", "Honours"));
-        courses.add(new Course("Software Engineering Principles", "Honours"));
+        courses.add(new Course("CSC512 Computer networks", "Honours"));
+        courses.add(new Course("CSC513 Distributed and parallel computing", "Honours"));
+        courses.add(new Course("CSC515 Advanced Java", "Honours"));
+        courses.add(new Course("CSC516 E-business fundamentals", "Honours"));
     }
 
     // Method to get all courses
