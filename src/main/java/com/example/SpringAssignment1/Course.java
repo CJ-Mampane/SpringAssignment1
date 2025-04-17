a5e2dcb63d7c6e999a47fa333cce4f9b1a13ef29
EsonaMadlalisa <esonaesona21@gmail.com>
2025-04-17 17:31:33 +0200

    Update Course.java

--- src/main/java/com/example/SpringAssignment1/Course.java
+++ src/main/java/com/example/SpringAssignment1/Course.java
@@ -1,26 +1,40 @@
 package com.example.SpringAssignment1;
 
 public class Course {
-
+    private Long id;
     private String name;
     private String category;
 
-    public Course(String name, String category) {
-            this.name = name;
-            this.category = category;
+    public Course() {}
+
+    public Course(Long id, String name, String category) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+    }
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public void setName(String name) {
         this.name = name;
     }
-    public void setCategory(String category) {
-        this.category = category;
+
+    public String getCategory() {
+        return category;
     }
 
-    public String getName() {
-            return name;
-    }
-    public String getCategory() {
-            return category;
+    public void setCategory(String category) {
+        this.category = category;
     }
 }
